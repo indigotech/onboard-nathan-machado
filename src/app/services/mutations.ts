395355e4e5c3ddcfb899d8abbda9701053c87ce8
Nathan Fabiano Machado <nathanfabiano@hotmail.com>
2022-03-11 14:27:39 -0300

    Login back-end integration

---
+++ src/app/services/mutations.ts
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+
+export const LoginMutation = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(data: {
+      email: $email,
+      password: $password
+    }) {
+      token
+    }
+  }
+`;
