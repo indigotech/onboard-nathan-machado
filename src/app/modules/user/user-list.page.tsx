e1e92d883a9f21b68000e3140d57749362e2ec03
Nathan Fabiano Machado <nathanfabiano@hotmail.com>
2022-03-16 14:34:14 -0300

    Users list pagination

--- src/app/modules/user/user-list.page.tsx
+++ src/app/modules/user/user-list.page.tsx
@@ -1,10 +1,12 @@
+import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { UserRow } from 'app/components/user-row';
-import { USER_LIST_TITLES } from 'app/constants';
+import { PAGINATION_LIMIT, USER_LIST_TITLES } from 'app/constants';
 import { UserListQuery } from 'app/services';
+import { AlertMsg } from 'atomic/mol.alert-msg';
 import { Table } from 'atomic/mol.table';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { getCurrentOffset, Paginator } from 'app/components/paginator';
 
 interface UserData {
   id: string;
@@ -13,25 +15,37 @@ interface UserData {
 }
 
 export function UserListPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [errorMsg, setErrorMsg] = useState('');
+  const currentOffset = getCurrentOffset(searchParams);
   const { loading, error, data } = useQuery(UserListQuery, {
-    variables: { offset: 0, limit: 20 },
+    variables: { offset: currentOffset, limit: PAGINATION_LIMIT },
   });
   const navigate = useNavigate();
 
   useEffect(() => {
     if (error?.graphQLErrors[0].name == 'UnauthorizedError') {
       navigate('/login');
+    } else if (error) {
+      setErrorMsg(error.message);
+    } else {
+      setErrorMsg('');
     }
   }, [error]);
 
   return (
     <>
       <h1>Users list</h1>
+
       <Table isLoading={loading} titles={USER_LIST_TITLES}>
         {data?.users?.nodes.map(({ id, name, email }: UserData) => {
           return <UserRow key={id} name={name} email={email} />;
         })}
       </Table>
+
+      <Paginator isLoading={loading} count={data?.users?.count} limit={data?.users?.pageInfo.limit} />
+
+      {errorMsg && <AlertMsg type='error'>{errorMsg}</AlertMsg>}
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
-import { client } from './app/services';
+import { apolloClient } from './app/services';
 import { AppRoutes } from 'routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ApolloProvider client={client()}>
+      <ApolloProvider client={apolloClient}>
         <AppRoutes />
       </ApolloProvider>
     </BrowserRouter>
