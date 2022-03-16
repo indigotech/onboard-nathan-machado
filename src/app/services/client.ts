9700ba08fd741ee08efdd1d761331884c87e2d98
Nathan Fabiano Machado <nathanfabiano@hotmail.com>
2022-03-16 11:03:27 -0300

    Apollo client adjustments

--- src/app/services/client.ts
+++ src/app/services/client.ts
@@ -1,24 +1,22 @@
-import { ApolloClient, createHttpLink, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { AUTH_TOKEN_KEY } from 'app/constants';
 
-export function client(): ApolloClient<NormalizedCacheObject> {
-  const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem(AUTH_TOKEN_KEY);
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? token : '',
-      },
-    };
-  });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN_KEY);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : '',
+    },
+  };
+});
 
-  const httpLink = createHttpLink({
-    uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
-  });
+const httpLink = createHttpLink({
+  uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
+});
 
-  return new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-}
+export const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
