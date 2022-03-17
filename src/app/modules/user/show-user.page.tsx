db18cca102293086dda39e2cf6ee9dd28e3b8371
Nathan Fabiano Machado <nathanfabiano@hotmail.com>
2022-03-17 16:28:48 -0300

    Show User page integration

--- src/app/modules/user/show-user.page.tsx
+++ src/app/modules/user/show-user.page.tsx
@@ -1,3 +1,36 @@
+import { useQuery } from '@apollo/client';
+import { UserDetailsQuery } from 'app/services';
+import { useParams } from 'react-router-dom';
+import Loading from 'react-loading';
+import { InfoLine } from 'app/components/info-line';
+import { AlertMsg } from 'atomic/mol.alert-msg';
+
 export function ShowUserPage() {
-  return <h1>User Details Page</h1>;
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(UserDetailsQuery, {
+    variables: { id },
+  });
+
+  return (
+    <>
+      {loading ? (
+        <Loading type='bars' color='#000' height={20} width={20} />
+      ) : (
+        <>
+          {error ? (
+            <AlertMsg type='error'>{error.message}</AlertMsg>
+          ) : (
+            <div>
+              <h3>{`User #${data.user.id}`}</h3>
+              <InfoLine title='Name' text={data.user.name} />
+              <InfoLine title='E-mail' text={data.user.email} />
+              <InfoLine title='Phone' text={data.user.phone} />
+              <InfoLine title='Birth Date' text={new Date(data.user.birthDate).toLocaleDateString('pt-BR')} />
+              <InfoLine title='Role' text={data.user.role} />
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
 }
